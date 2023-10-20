74dc564c3ed7c0cbd1c2bfd5d41b527e42694640
Olga Melman <olga.melman198237@gmail.com>
2023-10-20 21:33:10 +0300

    added code in task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+
+   return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+
+
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(users, gender) { // const people, ""
+   let filterNewArr = users.filter(user => user.gender == gender).map(i => i.age);
+   if (filterNewArr.length === 0) {
+      return 0;
+   }
+   const totalSumAge = filterNewArr.reduce((acc, item) => acc + item);
+   const averageAge = totalSumAge / filterNewArr.length;
+
+   return averageAge;
 }
