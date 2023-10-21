8e353f7fa40917d2521088d61aa37b7809edae76
Olga Melman <olga.melman198237@gmail.com>
2023-10-22 00:13:44 +0300

    added code in task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksArgs) {
+  if (!this.marks) return;
+
+  this.marks.push(...marksArgs);
+
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) return 0;
+
+
+  const averageMarks = this.marks.reduce((all, mark) => all + mark) / this.marks.length;
+  return averageMarks;
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
